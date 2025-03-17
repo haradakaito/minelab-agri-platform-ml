8a7d4faec25cf56f7a6bd587fc3fa4964ef13c70
csi <csi@users.noreply.github.com>
2025-03-17 15:28:15 +0900

    [add] ダウンロード先のフォルダ構成を修正

--- download_image.py
+++ download_image.py
@@ -1,21 +1,24 @@
+import json
 from lib import AWSHandler, ErrorHandler, Util
 
 if __name__ == '__main__':
     try:
+        # 設定ファイルの読み込み
+        with open(f"{Util.get_root_dir()}/config/config.json") as f:
+            config = json.load(f)
+
         # AWSハンドラを初期化
         aws_handler = AWSHandler(region_name='ap-northeast-1', bucket_name='minelab-iot-storage')
 
-        # 時間範囲を設定（現在時刻から24時間以内）
-        start_time = Util.get_timestamp(delta_hour=-24)
-        end_time   = Util.get_timestamp()
-
-        # 指定した時間範囲のオブジェクトをダウンロード
-        aws_handler.download_s3_objects_within_time_range(
-            base_prefix    = 'projects/csi/image-data/',                # プレフィックス
-            local_base_dir = f'{Util.get_root_dir()}/data/image-data/', # ローカルディレクトリ
-            start_time     = start_time,                                # 開始時間
-            end_time       = end_time                                   # 終了時間
-        )
+        for field_device in config["FieldDevice"]["Image"]:
+            Util.create_path(path=f'{Util.get_root_dir()}/data/image-data/{field_device}')
+            # 指定した時間範囲のオブジェクトをダウンロード
+            aws_handler.download_s3_objects(
+                remote_path = f'projects/csi/image-data/{field_device}/',               # S3のプレフィックス
+                local_path  = f'{Util.get_root_dir()}/data/image-data/{field_device}/', # ローカルパス
+                start_time  = Util.get_timestamp(delta_hour=-24),                       # 開始時間
+                end_time    = Util.get_timestamp()                                      # 終了時間
+            )
 
     except Exception as e:
         # エラーハンドラを初期化
--- lib/aws_handler.py
+++ lib/aws_handler.py
@@ -1,7 +1,6 @@
 import os
 import boto3
-from datetime import datetime
-from dateutil import parser
+from datetime import datetime as dt
 
 class AWSHandler:
     """AWSの各種サービスを操作するためのハンドラクラス"""
@@ -12,118 +11,92 @@ class AWSHandler:
         self.s3_bucket_name = bucket_name
 
     def _list_s3_objects(self, prefix: str) -> list:
-        """
-        指定したS3プレフィックス内のすべてのオブジェクト（ファイル）一覧を取得
-
-        params
-        ------
-        prefix: str
-            S3のプレフィックス（ディレクトリ）パス
-
-        return
-        ------
-        objects: list
-            オブジェクト一覧（dictのリスト）
-        """
-        objects = []
-        continuation_token = None
-        # ページネーションを使ってオブジェクト一覧を取得
-        while True:
-            # 継続トークンを指定してリクエスト
-            if continuation_token:
-                response = self.s3_client.list_objects_v2(
-                    Bucket = self.s3_bucket_name,          # バケット名
-                    Prefix = prefix,                       # プレフィックス
-                    ContinuationToken = continuation_token # 継続トークン
-                )
-            else:
-                response = self.s3_client.list_objects_v2(
-                    Bucket = self.s3_bucket_name, # バケット名
-                    Prefix = prefix               # プレフィックス
-                )
-
-            # レスポンスからオブジェクト一覧を取得
-            if 'Contents' in response:
-                objects.extend(response['Contents'])
-            # 継続トークンがあれば次のページを取得
-            if response.get('IsTruncated'):
-                continuation_token = response['NextContinuationToken']
-            else:
-                break
-        return objects
-
-    def _validation_time_range(self, start_time: str, end_time: str) -> bool:
-        """指定した時間範囲のバリデーションを行い，問題がなければ True を返す"""
+        """指定したS3プレフィックス内のすべてのオブジェクト（ファイル）一覧を取得"""
         try:
-            start_dt = datetime.strptime(start_time, "%Y-%m-%dT%H-%M-%S")
-            end_dt   = datetime.strptime(end_time,   "%Y-%m-%dT%H-%M-%S")
-            return all([
-                start_dt <= end_dt,
-                0 <= start_dt.year   <= 9999, 0 <= end_dt.year   <= 9999, # 年は0以上9999以下
-                1 <= start_dt.month  <= 12,   1 <= end_dt.month  <= 12,   # 月は1以上12以下
-                1 <= start_dt.day    <= 31,   1 <= end_dt.day    <= 31,   # 日は1以上31以下
-                0 <= start_dt.hour   <= 23,   0 <= end_dt.hour   <= 23,   # 時間は0以上23以下
-                0 <= start_dt.minute <= 59,   0 <= end_dt.minute <= 59,   # 分は0以上59以下
-                0 <= start_dt.second <= 59,   0 <= end_dt.second <= 59    # 秒は0以上59以下
-            ])
+            objects            = []   # オブジェクト一覧
+            continuation_token = None # 継続トークン
 
-        except Exception:
-            return False
+            # ページネーションを使ってオブジェクト一覧を取得
+            while True:
+                # 継続トークンを指定してリクエスト
+                if continuation_token:
+                    response = self.s3_client.list_objects_v2(Bucket=self.s3_bucket_name, Prefix=prefix, ContinuationToken=continuation_token)
+                else:
+                    response = self.s3_client.list_objects_v2(Bucket=self.s3_bucket_name, Prefix=prefix)
 
-    def download_s3_objects_within_time_range(self, base_prefix: str, local_base_dir: str, start_time: str, end_time: str):
+                # レスポンスからオブジェクト一覧を取得
+                if 'Contents' in response:
+                    objects.extend(response['Contents'])
+                # 継続トークンの確認（次のページがあるか）
+                if response.get('IsTruncated'):
+                    continuation_token = response['NextContinuationToken']
+                else:
+                    break
+            return objects
+        except Exception as e:
+            raise e
+
+    def _validation_time_range(self, start_time: str, end_time: str, timestamp_format: str) -> bool:
+        """時間範囲のバリデーション"""
+        start_dt = dt.strptime(start_time, timestamp_format)
+        end_dt   = dt.strptime(end_time,   timestamp_format)
+        return all([
+            start_dt <= end_dt,
+            0 <= start_dt.year   <= 9999, 0 <= end_dt.year   <= 9999, # 年は0以上9999以下
+            1 <= start_dt.month  <= 12,   1 <= end_dt.month  <= 12,   # 月は1以上12以下
+            1 <= start_dt.day    <= 31,   1 <= end_dt.day    <= 31,   # 日は1以上31以下
+            0 <= start_dt.hour   <= 23,   0 <= end_dt.hour   <= 23,   # 時間は0以上23以下
+            0 <= start_dt.minute <= 59,   0 <= end_dt.minute <= 59,   # 分は0以上59以下
+            0 <= start_dt.second <= 59,   0 <= end_dt.second <= 59    # 秒は0以上59以下
+        ])
+
+    def download_s3_objects(self, remote_path: str, local_path: str, start_time: str, end_time: str) -> None:
         """
-        指定した時間範囲のオブジェクトをローカルにダウンロードする
+        指定した時間範囲内のS3オブジェクトをダウンロード
 
         params
         ------
-        base_prefix: str
-            S3のプレフィックス（ディレクトリ）パス
-        local_base_dir: str
-            ダウンロード先のローカルディレクトリパス
+        remote_path: str
+            S3のプレフィックス
+        local_path: str
+            ローカルパス
         start_time: str
-            ダウンロード開始時間
+            開始時間
         end_time: str
-            ダウンロード終了時間
+            終了時間
 
         return
         ------
         None
         """
 
-        # 時間範囲のバリデーション
-        if not self._validation_time_range(start_time, end_time):
-            raise ValueError("時間範囲が不正です")
+        timestamp_format = "%Y-%m-%dT%H-%M-%S" # タイムスタンプのフォーマット
 
-        # 指定したプレフィックス内の全オブジェクトを取得
-        for object in self._list_s3_objects(prefix=base_prefix):
-            s3_key = object['Key']
-            # S3キーから日付情報を抽出
-            try:
-                filename      = os.path.basename(s3_key)                              # ファイル名
-                timestamp_str = filename.split('.')[0]                                # タイムスタンプ部分
-                file_dt       = datetime.strptime(timestamp_str, "%Y-%m-%dT%H-%M-%S") # ファイルの日時
-            except Exception as e:
-                print(f"エラー: {s3_key} -> {e}")
-                continue
+        try:
+            # 時間範囲のバリデーション
+            if not self._validation_time_range(start_time=start_time, end_time=end_time, timestamp_format=timestamp_format):
+                raise ValueError("時間範囲が不正です")
 
-            # 指定した時間範囲内のファイルのみ処理
-            if datetime.strptime(start_time, "%Y-%m-%dT%H-%M-%S") <= file_dt <= datetime.strptime(end_time, "%Y-%m-%dT%H-%M-%S"):
-                relative_path   = s3_key[len(base_prefix):]                   # プレフィックスを除いた相対パス
-                local_file_path = os.path.join(local_base_dir, relative_path) # ローカルファイルパス
-                # 必要なディレクトリを作成
-                os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
-                # ダウンロード
-                self.s3_client.download_file(self.s3_bucket_name, s3_key, local_file_path)
-                # print(f"Downloaded: {s3_key} -> {local_file_path}")
+            # 指定したプレフィックス内の全オブジェクトを取得
+            for object in self._list_s3_objects(prefix=remote_path):
+                filename_ext = os.path.basename(object['Key'])                           # ファイル名（拡張子あり）
+                timestamp    = dt.strptime(filename_ext.split('.')[0], timestamp_format) # ファイル名（タイムスタンプ）を取得
+
+                # 指定した時間範囲内のファイルのみダウンロード（ローカルパスに保存）
+                print(object['Key'])
+                if dt.strptime(start_time, timestamp_format) <= timestamp <= dt.strptime(end_time, timestamp_format):
+                    self.s3_client.download_file(self.s3_bucket_name, object['Key'], f"{local_path}{filename_ext}")
+        except Exception as e:
+            raise e
 
 # 使用例
 if __name__ == "__main__":
     # AWSハンドラの初期化
     aws_handler = AWSHandler(region_name="ap-northeast-1", bucket_name="minelab-iot-storage")
     # 指定した時間範囲のオブジェクトをダウンロード
-    aws_handler.download_s3_objects_within_time_range(
-        base_prefix    = "projects/csi/image-data/",
-        local_base_dir = "/home/csi/minelab-agri-platform-ml/__dev__/harada/data/image-data/",
-        start_time     = "2025-03-12T00-00-00",
-        end_time       = "2025-03-13T00-00-00"
+    aws_handler.download_s3_objects(
+        remote_path = "projects/csi/image-data/minelab-iot-camera-1",
+        local_path  = "..data/image-data/minelab-iot-camera-1/",
+        start_time  = "2025-03-13T00-00-00",
+        end_time    = "2025-03-14T00-00-00"
     )

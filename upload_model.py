1c28a4fe3f89447b34d741057aa91dea1eb1e259
csi <csi@users.noreply.github.com>
2025-03-17 16:02:19 +0900

    [fix] モデルアップロードのコード

--- upload_model.py
+++ upload_model.py
@@ -1,9 +1,14 @@
+import json
 from lib import AWSHandler, ErrorHandler, Util
 
 if __name__ == '__main__':
     try:
+        # 設定ファイルの読み込み
+        with open(f"{Util.get_root_dir()}/config/config.json") as f:
+            config = json.load(f)
+
         # AWSハンドラを初期化
-        aws_handler = AWSHandler()
+        aws_handler = AWSHandler(region_name='ap-northeast-1', bucket_name='minelab-iot-storage')
 
     except Exception as e:
         # エラーハンドラを初期化
